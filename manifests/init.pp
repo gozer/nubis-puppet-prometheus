af00f54d8b9ab590d422169c9b33770524270e15
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-19 15:20:34 -0500

    upstart

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,62 +50,114 @@ class nubis_prometheus($version = '1.4.1', $blackbox_version = '0.3.0', $tag_nam
   $prometheus_url = "https://github.com/prometheus/prometheus/releases/download/v${version}/prometheus-${version}.linux-amd64.tar.gz"
   $blackbox_url = "https://github.com/prometheus/blackbox_exporter/releases/download/v${blackbox_version}/blackbox_exporter-${blackbox_version}.linux-amd64.tar.gz"
 
-file { '/opt/prometheus':
-  ensure => 'directory',
-  owner  => 0,
-  group  => 0,
-  mode   => '0755',
-}
+  file { '/opt/prometheus':
+    ensure => 'directory',
+    owner  => 0,
+    group  => 0,
+    mode   => '0755',
+  }
 
-file { '/etc/prometheus':
-  ensure => 'directory',
-  owner  => 0,
-  group  => 0,
-  mode   => '0755',
-}->
-file { '/etc/prometheus/rules.d':
-  ensure => 'directory',
-  owner  => 0,
-  group  => 0,
-  mode   => '0755',
-}
-file { '/etc/prometheus/nubis.rules.d':
-  ensure => 'directory',
-  owner  => 0,
-  group  => 0,
-  mode   => '0755',
-}
-file { '/var/lib/prometheus':
-  ensure => 'directory',
-  owner  => 0,
-  group  => 0,
-  mode   => '0755',
-}
+  file { '/etc/prometheus':
+    ensure => 'directory',
+    owner  => 0,
+    group  => 0,
+    mode   => '0755',
+  }->
+  file { '/etc/prometheus/rules.d':
+    ensure => 'directory',
+    owner  => 0,
+    group  => 0,
+    mode   => '0755',
+  }
+  file { '/var/lib/prometheus':
+    ensure => 'directory',
+    owner  => 0,
+    group  => 0,
+    mode   => '0755',
+  }
 
-notice ("Grabbing prometheus ${version}")
-staging::file { "prometheus.${version}.tar.gz":
-  source => $prometheus_url,
-}->
-staging::extract { "prometheus.${version}.tar.gz":
-  strip   => 1,
-  target  => '/opt/prometheus',
-  creates => '/opt/prometheus/prometheus',
-  require => File['/opt/prometheus'],
-}
+  notice ("Grabbing prometheus ${version}")
+  staging::file { "prometheus.${version}.tar.gz":
+    source => $prometheus_url,
+  }->
+  staging::extract { "prometheus.${version}.tar.gz":
+    strip   => 1,
+    target  => '/opt/prometheus',
+    creates => '/opt/prometheus/prometheus',
+    require => File['/opt/prometheus'],
+  }
 
-notice ("Grabbing blackbox ${blackbox_version}")
-staging::file { "blackbox.${blackbox_version}.tar.gz":
-  source => $blackbox_url,
-}->
-staging::extract { "blackbox.${blackbox_version}.tar.gz":
-  strip   => 1,
-  target  => '/usr/local/bin',
-  creates => '/usr/local/bin/blackbox_exporter',
-}
+  notice ("Grabbing blackbox ${blackbox_version}")
+  staging::file { "blackbox.${blackbox_version}.tar.gz":
+    source => $blackbox_url,
+  }->
+  staging::extract { "blackbox.${blackbox_version}.tar.gz":
+    strip   => 1,
+    target  => '/usr/local/bin',
+    creates => '/usr/local/bin/blackbox_exporter',
+  }
 
-include 'upstart'
+  include 'upstart'
 
-upstart::job { 'blackbox':
+  upstart::job { 'prometheus':
+    description    => 'Prometheus',
+    service_ensure => 'stopped',
+    # Never give up
+    respawn        => true,
+    respawn_limit  => 'unlimited',
+    start_on       => '(local-filesystems and net-device-up IFACE!=lo)',
+    env            => {
+      'SLEEP_TIME' => 1,
+      'GOMAXPROCS' => 2,
+    },
+    user           => 'root',
+    group          => 'root',
+    script         => '
+  if [ -r /etc/profile.d/proxy.sh ]; then
+    echo "Loading Proxy settings"
+    . /etc/profile.d/proxy.sh
+  fi
+
+  exec /usr/local/bin/blackbox_exporter -config.file /etc/prometheus/blackbox.yml -log.level info -log.format "logger:syslog?appname=blackbox_exporter&local=7"
+',
+    pre_start      => '
+  if [ "$BACKUP" != "SKIP" ]; then
+    if [ -r /var/lib/prometheus/PRISTINE ]; then
+      echo "Restoring backup from S3 before startup..."
+      /usr/local/bin/nubis-prometheus-backup restore
+      echo " Done"
+    fi
+  fi
+  initctl unset-env BACKUP
+  unset BACKUP
+'
+    post_stop      => '
+  goal=$(initctl status $UPSTART_JOB | awk '{print $2}' | cut -d '/' -f 1)
+  # only backup on explicit stop action, not crashes and the like
+  if [ "$goal" = "stop" ]; then
+    if [ "$BACKUP" != "SKIP" ]; then
+      echo -n "Backing up to S3..."
+      /usr/local/bin/nubis-prometheus-backup save
+      echo " Done"
+   fi
+  else
+    echo "Backoff for $SLEEP_TIME seconds"
+    sleep $SLEEP_TIME
+    NEW_SLEEP_TIME=`expr 2 \* $SLEEP_TIME`
+    if [ $NEW_SLEEP_TIME -ge 60 ]; then
+        NEW_SLEEP_TIME=60
+    fi
+    initctl set-env SLEEP_TIME=$NEW_SLEEP_TIME
+  fi
+  initctl unset-env BACKUP
+  unset BACKUP
+',
+  post_start       => '
+initctl set-env SLEEP_TIME=1
+'
+  }
+
+  upstart::job { 'blackbox':
     description    => 'Prometheus Blackbox Exporter',
     service_ensure => 'stopped',
     # Never give up
@@ -138,6 +190,6 @@ if [ $goal != "stop" ]; then
     initctl set-env SLEEP_TIME=$NEW_SLEEP_TIME
 fi
 ',
-}
+  }
 
 }

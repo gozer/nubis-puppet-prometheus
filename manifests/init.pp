72b8f76873fec5f0211c83fdac33f43fede378dc
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-20 10:56:12 -0500

    add backup script

--- manifests/init.pp
+++ manifests/init.pp
@@ -77,6 +77,14 @@ class nubis_prometheus($version = '1.4.1', $blackbox_version = '0.3.0', $tag_nam
     group  => 0,
     mode   => '0755',
   }
+  
+  file { '/usr/local/bin/nubis-prometheus-backup':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => "puppet:///modules/${module_name}/backup",
+  }
 
   notice ("Grabbing prometheus ${version}")
   staging::file { "prometheus.${version}.tar.gz":
